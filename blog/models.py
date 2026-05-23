588de260cc852f44a0a1eaa0a2f8f9c2e161c736
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-05-23 11:54:32 +0900

    モデル追加 (Category, Post)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from customauth.models import CustomUser
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    slug = models.CharField(max_length=20, unique=True)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    thumbnail = models.ImageField(null=True, blank=True)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
