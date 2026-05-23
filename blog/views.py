6c9704a928679ad87651a340817d4468b65c798a
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-05-23 16:57:36 +0900

    ページネーション追加

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 
 class PostListView(generic.ListView):
     template_name = 'blog/posts/index.html'
+    paginate_by = 10
 
     def get_queryset(self):
         self.queryset = Post.objects.select_related('category').order_by('-posted_at')
