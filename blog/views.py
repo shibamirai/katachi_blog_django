89361da6e303b1e833335f3d91170e60b59950ab
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-05-23 10:58:28 +0900

    Bootstrap の導入

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse("test")
+    return render(request, 'base.html')
--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'customauth',
     'blog',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
