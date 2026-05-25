c4d4e28ed8e5652095b56f4b0c85858d8e1e8cbd
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-05-25 13:13:47 +0900

    記事個別表示機能追加

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='home')
+    path('', views.PostListView.as_view(), name='home'),
+    path('posts/<slug:slug>', views.PostDetailView.as_view(), name='detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,13 @@ class PostListView(generic.ListView):
     def get_queryset(self):
         self.queryset = Post.objects.select_related('category').order_by('-posted_at')
         return super().get_queryset()
+
+
+class PostDetailView(generic.DetailView):
+    template_name = 'blog/posts/detail.html'
+    queryset = Post.objects.select_related('category').select_related('author')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_posts'] = Post.objects.filter(category_id=self.object.category_id).order_by('-posted_at')[:5]
+        return context
